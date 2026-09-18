84ac5135bae3706db2b2186e3c3fccc13775c847
dust2ash7 <seanified@me.com>
2026-09-18 10:41:46 -0700

    Restore game script and route music to Happy Clappy Loop

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-PLACEHOLDER
+(() => {
+  const OrigAudio = window.Audio;
+  window.Audio = function (src) {
+    if (src && /music-bed/i.test(String(src))) {
+      src = "https://opengameart.org/sites/default/files/HappyClappyLoop.wav";
+    }
+    return new OrigAudio(src);
+  };
+  window.Audio.prototype = OrigAudio.prototype;
+  const s = document.createElement("script");
+  s.src = "https://cdn.jsdelivr.net/gh/dust2ash7/2048-puzzle@3334884e6270af27cb15e5a2d19f19af547391d9/script.js";
+  s.defer = true;
+  document.head.appendChild(s);
+})();
